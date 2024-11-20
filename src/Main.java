b0ab55aeb182f7f584297a273264a8c4a0a33aba
kaylinanewman <kaylinanewman@gmail.com>
2024-11-20 18:20:05 -0500

    15 times now

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 15; i++) {
             System.out.println("Hello World!");
             Thread.sleep(2000);
         }
